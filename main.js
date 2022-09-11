439b63c0db6862ccdabb9834b0978f4c385001d6
plasmatiger <aakshat84@gmail.com>
2022-09-11 22:41:18 +0530

    added deeplink data to local storage

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow, shell, dialog } = require("electron");
 const path = require("path");
+var url = require("url");
+const storage = require("electron-json-storage");
+
+const dataPath = storage.getDataPath();
+console.log(dataPath);
+
+const getQuery = (query) => {};
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -9,6 +16,8 @@ const createWindow = () => {
       preload: path.join(__dirname, "preload.js"),
     },
   });
+  //   mainWindow.webContents.openDevTools();
+  win.webContents.openDevTools();
   if (process.defaultApp) {
     if (process.argv.length >= 2) {
       app.setAsDefaultProtocolClient("instaminutes", process.execPath, [
@@ -18,6 +27,16 @@ const createWindow = () => {
   } else {
     app.setAsDefaultProtocolClient("instaminutes");
   }
+
+  storage.get("userInfo", function (error, data) {
+    if (error) throw error;
+    console.log("inside create window looking for state variables");
+    console.log("userInfo", data);
+    console.log("name", data.name);
+    console.log("code", data.code);
+    console.log("workspace", data.workspace);
+    console.log("done searching");
+  });
 
   win.loadFile("index.html");
 };
@@ -29,8 +48,27 @@ app.whenReady().then(() => {
   });
 });
 
-app.on("open-url", (event, url) => {
-  dialog.showErrorBox("Welcome Back", `You arrived from: ${url}`);
+app.on("open-url", (event, deeplinkUrl) => {
+  console.log("url for deep-link----" + deeplinkUrl);
+  let convertedUrl = url.parse(deeplinkUrl, true);
+  //   let convertedSearch = convertedUrl.search;
+  //   let convertedPath = convertedUrl.pathname;
+  let convertedQuery = convertedUrl.query;
+  console.log(`query object`, convertedQuery);
+  storage.set("userInfo", convertedQuery);
+
+  //   storage.set("name", convertedQuery.name);
+  //   storage.set("code", convertedQuery.code);
+  //   storage.set("workspace", convertedQuery.workspace);
+
+  //   console.log("query# " + convertedSearch);
+  //   console.log("path# " + convertedPath);
+
+  //instaminutes:///path1/path2/path3?name=akshat&workspace=6789654&code=89764adsfsghjjkk
+  //   dialog.showErrorBox(
+  //     "Welcome Back",
+  //     `You arrived from: ${url} with event ${event}`
+  //   );
 });
 
 app.on("window-all-closed", () => {
